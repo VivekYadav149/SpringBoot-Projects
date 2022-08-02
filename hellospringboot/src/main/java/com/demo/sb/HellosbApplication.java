c5994bebae82786bf168c788fed356c1e81c795f
Vivek Yadav <vivek.yadav@adidas.com>
2022-08-02 23:20:36 +0530

    First Demo Project HelloSpringBoot

---
+++ hellospringboot/src/main/java/com/demo/sb/HellosbApplication.java
@@ -0,0 +1,13 @@
+package com.demo.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellosbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellosbApplication.class, args);
+	}
+
+}
