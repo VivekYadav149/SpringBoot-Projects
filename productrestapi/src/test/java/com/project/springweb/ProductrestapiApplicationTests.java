b39e1ce840e99c24a5dad774f347c8c5a99dea4b
Vivek Yadav <vivek.yadav@adidas.com>
2022-08-04 10:57:57 +0530

    Configure REST URL

--- productrestapi/src/test/java/com/project/springweb/ProductrestapiApplicationTests.java
+++ productrestapi/src/test/java/com/project/springweb/ProductrestapiApplicationTests.java
@@ -2,6 +2,7 @@ package com.project.springweb;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
@@ -13,10 +14,13 @@ import com.project.springweb.entities.Product;
 @SpringBootTest
 public class ProductrestapiApplicationTests {
 
+	@Value("${productrestapi.services.url}")
+	private String baseURL;
+	
 	@Test
 	public void testGetProduct() {
 		RestTemplate restTemplate = new RestTemplate();
-		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/1", Product.class);
+		Product product = restTemplate.getForObject(baseURL +"1", Product.class);
 		assertNotNull(product);
 		assertEquals("IPhone", product.getName());
 	}
@@ -28,7 +32,7 @@ public class ProductrestapiApplicationTests {
 		product.setName("Samsung Mobile");
 		product.setDescription("Its new Samsung Phone");
 		product.setPrice(1299);
-		Product newProduct = restTemplate.postForObject("http://localhost:8080/productapi/products/", product, Product.class);
+		Product newProduct = restTemplate.postForObject(baseURL, product, Product.class);
 		assertNotNull(newProduct);
 		assertNotNull(newProduct.getId());
 		assertEquals("Samsung Mobile", newProduct.getName());
@@ -37,10 +41,10 @@ public class ProductrestapiApplicationTests {
 	@Test
 	public void testUpdateProduct() {
 		RestTemplate restTemplate = new RestTemplate();
-		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/4", Product.class);
+		Product product = restTemplate.getForObject(baseURL + "1", Product.class);
 		product.setPrice(1978);
 		product.setName("Samsung Galaxy S22");
 		product.setDescription("S22 series launched");
-		restTemplate.put("http://localhost:8080/productapi/products/", product);
+		restTemplate.put(baseURL, product);
 	}
 }
